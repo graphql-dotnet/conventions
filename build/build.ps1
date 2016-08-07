e55c99440872355b3ef2f9e40b4ca86594e687be
Magdziak Marek <marek.magdziak@tieto.com>
2016-08-07 16:24:51 +0200

    fix(Appveyor): build versioning

--- build/build.ps1
+++ build/build.ps1
@@ -15,6 +15,10 @@ properties {
   $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
   $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
+  if ($env:APPVEYOR_REPO_BRANCH -eq "develop") {
+    $nugetPrerelease = "build" + $revision
+  }
+
   $baseDir  = resolve-path ..
   $buildDir = "$baseDir\Build"
   $sourceDir = "$baseDir\Src"
@@ -264,11 +268,6 @@ function GetNuGetVersion()
   if ($nugetPrerelease -ne $null)
   {
     $nugetVersion = $nugetVersion + "-" + $nugetPrerelease
-    $nugetVersion = $nugetVersion + "-" + $revision
-  }
-  else
-  {
-      $nugetVersion = $nugetVersion + "." + $revision
   }
 
   return $nugetVersion
