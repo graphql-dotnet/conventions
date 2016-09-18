4359e40cb9addd8521f711e7c868c728438496c6
Randy Ridge <randyridge@gmail.com>
2016-09-18 15:10:28 -0400

    added benchmark app

---
+++ src/GraphQLParser.Benchmarks/DefaultBenchmarkConfig.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+
+namespace GraphQLParser.Benchmarks {
+    public sealed class DefaultBenchmarkConfig : ManualConfig {
+        public DefaultBenchmarkConfig() {
+            Add(Job.Default
+                .With(Runtime.Clr)
+                .With(Jit.RyuJit)
+                .WithLaunchCount(3)
+                .WithWarmupCount(5)
+                .WithTargetCount(10)
+            );
+
+            #if NET45
+            Add(new BenchmarkDotNet.Diagnostics.Windows.MemoryDiagnoser());
+            #endif
+
+            Add(Job.Default
+                .With(Runtime.Core)
+                .With(Jit.RyuJit)
+                .WithLaunchCount(3)
+                .WithWarmupCount(5)
+                .WithTargetCount(10)
+            );
+
+            Add(StatisticColumn.AllStatistics);
+        }
+    }
+}
---
+++ src/GraphQLParser.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace GraphQLParser.Benchmarks {
+    internal static class Program {
+        private static void Main() {
+            var benchmarksAssembly = typeof(Program).GetTypeInfo().Assembly;
+            var benchmarkSwitcher = new BenchmarkSwitcher(benchmarksAssembly);
+            benchmarkSwitcher.Run();
+        }
+    }
+}
