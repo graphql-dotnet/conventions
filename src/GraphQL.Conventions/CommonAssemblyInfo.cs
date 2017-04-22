8b971e4525fced03bd37c4de87e959a9b47d04e0
Tommy Lillehagen <tommy@lillehagen.me>
2017-04-22 08:20:30 +0100

    Allow request payload to be wrapped in 'data' element

--- src/GraphQL.Conventions/CommonAssemblyInfo.cs
+++ src/GraphQL.Conventions/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright 2016-2017 Tommy Lillehagen. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.3")]
-[assembly: AssemblyInformationalVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.4")]
+[assembly: AssemblyInformationalVersion("1.2.4")]
 [assembly: CLSCompliant(false)]
 
 [assembly: InternalsVisibleTo("GraphQL.Conventions.Tests")]
--- test/Tests/Web/RequestTests.cs
+++ test/Tests/Web/RequestTests.cs
@@ -16,6 +16,25 @@ namespace GraphQL.Conventions.Tests.Web
         }
 
         [Test]
+        public void Can_Instantiate_Request_Object_From_Data_String_Without_Variables()
+        {
+            var request = Request.New("{\"data\":{\"query\":\"{}\"}}");
+            request.IsValid.ShouldEqual(true);
+            request.QueryString.ShouldEqual("{}");
+            request.Variables.ShouldEqual(null);
+        }
+
+        [Test]
+        public void Can_Instantiate_Request_Object_From_Data_String_With_Variables()
+        {
+            var request = Request.New("{\"data\":{\"query\":\"{ foo(baz: 1) { bar } }\",\"variables\":{\"arg\":10}}}");
+            request.IsValid.ShouldEqual(true);
+            request.QueryString.ShouldEqual("{ foo(baz: 1) { bar } }");
+            request.Variables.ShouldNotEqual(null);
+            request.Variables["arg"].ShouldEqual(10);
+        }
+
+        [Test]
         public void Cannot_Instantiate_Request_Object_From_Invalid_String()
         {
             var request = Request.New("\"invalid_query\":\"{}\"");
