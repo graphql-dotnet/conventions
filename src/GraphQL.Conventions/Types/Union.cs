e91a804763602b68b179fb0d9f59ec7a8dc2754c
bourgoisa <alexander.bourgois@cognistreamer.com>
2017-07-20 12:01:39 +0100

    Extended union from 10 to 20 types

--- src/GraphQL.Conventions/Types/Union.cs
+++ src/GraphQL.Conventions/Types/Union.cs
@@ -24,4 +24,24 @@ namespace GraphQL.Conventions
     public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9> : Union { }
 
     public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19> : Union { }
+
+    public abstract class Union<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20> : Union { }
 }
