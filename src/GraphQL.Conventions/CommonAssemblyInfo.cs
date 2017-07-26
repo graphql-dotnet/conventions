9fe054d57ea7408c013d3e4b6cfbbb6149c2e518
Tommy Lillehagen <tommy@lillehagen.me>
2017-07-26 10:39:17 +0100

    Version 1.2.12

--- src/GraphQL.Conventions/CommonAssemblyInfo.cs
+++ src/GraphQL.Conventions/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright 2016-2017 Tommy Lillehagen. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.11")]
-[assembly: AssemblyInformationalVersion("1.2.11")]
+[assembly: AssemblyFileVersion("1.2.12")]
+[assembly: AssemblyInformationalVersion("1.2.12")]
 [assembly: CLSCompliant(false)]
 
 [assembly: InternalsVisibleTo("Tests")]
