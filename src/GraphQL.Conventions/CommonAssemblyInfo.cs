ff14c77ddada7fd7d84e0d1cc79170f7bbf8c747
Tommy Lillehagen <tommy@lillehagen.me>
2017-08-16 16:25:39 +0100

    Enable profiling through RequestBuilder

--- src/GraphQL.Conventions/CommonAssemblyInfo.cs
+++ src/GraphQL.Conventions/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright 2016-2017 Tommy Lillehagen. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.14")]
-[assembly: AssemblyInformationalVersion("1.2.14")]
+[assembly: AssemblyFileVersion("1.2.15")]
+[assembly: AssemblyInformationalVersion("1.2.15")]
 [assembly: CLSCompliant(false)]
 
 [assembly: InternalsVisibleTo("Tests")]
