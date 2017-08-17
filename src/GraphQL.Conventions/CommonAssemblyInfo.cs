8b5e2cc916a50d7e3e4b226cd13a25bf69a9d65e
Tommy Lillehagen <tommy@lillehagen.me>
2017-08-17 12:01:19 +0100

    Bump 1.3.2

--- src/GraphQL.Conventions/CommonAssemblyInfo.cs
+++ src/GraphQL.Conventions/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright 2016-2017 Tommy Lillehagen. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.1")]
-[assembly: AssemblyInformationalVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyInformationalVersion("1.3.2")]
 [assembly: CLSCompliant(false)]
 
 [assembly: InternalsVisibleTo("Tests")]
