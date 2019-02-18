3b4f233960153db72fe3c5aedaf1d8696e835d9d
Oleksii Biliachenko <o.biliachenko@sana-commerce.com>
2019-02-18 10:28:19 +0200

    Improve tests checking ExposeExceptions option

--- test/Tests/Adapters/Engine/ExposingExceptionsTests.cs
+++ test/Tests/Adapters/Engine/ExposingExceptionsTests.cs
@@ -2,6 +2,7 @@
 using GraphQL.Conventions.Tests;
 using GraphQL.Conventions.Tests.Templates;
 using GraphQL.Conventions.Tests.Templates.Extensions;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,12 +17,20 @@ namespace Tests.Adapters.Engine
         public async Task Will_not_expose_exceptions_when_not_enabled()
         {
             var engine = GraphQLEngine.New<Query>();
-            var result = await engine
+            GraphQL.ExecutionResult result = await engine
                 .NewExecutor()
                 .WithQueryString("query { queryData }")
                 .Execute();
 
             result.ExposeExceptions.ShouldBeFalse("By default exposing exceptions should be disabled.");
+
+            var message = JObject.FromObject(result)["errors"].First.Value<string>("message");
+
+            bool anyStackTracePart = message.Contains("at Tests.Adapters.Engine.ExposingExceptionsTests.Query.QueryData() in") ||
+                message.Contains("stack trace") ||
+                message.Contains("at");
+
+            anyStackTracePart.ShouldBeFalse($"There should be no stack trace in error messages when {nameof(result.ExposeExceptions)} is disabled.");
         }
 
         [Test]
@@ -35,8 +44,11 @@ namespace Tests.Adapters.Engine
                 .Execute();
 
             result.ExposeExceptions.ShouldBeTrue($"{nameof(result.ExposeExceptions)} should be enabled when {nameof(GraphQLEngine.WithExposedExceptions)} is called.");
+            JObject.FromObject(result)["errors"].First.Value<string>("message")
+                .Contains("at Tests.Adapters.Engine.ExposingExceptionsTests.Query.QueryData() in")
+                .ShouldBeTrue($"There should be stack trace in error messages when {nameof(result.ExposeExceptions)} is enabled.");
         }
-
+        
         class Query
         {
             public string QueryData() => throw new CustomException();
