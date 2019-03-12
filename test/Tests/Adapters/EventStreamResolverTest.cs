30494ab799461e4d1cabe570bc61e863caa46bec
Marco Hamann <marco.hamann@dmgmori.com>
2019-03-12 11:38:49 +0100

    added unittest for EventStreamResolver

---
+++ test/Tests/Adapters/EventStreamResolverTest.cs
@@ -0,0 +1,41 @@
+﻿using GraphQL.Conventions;
+using GraphQL.Conventions.Adapters.Resolvers;
+using GraphQL.Conventions.Tests;
+using GraphQL.Conventions.Tests.Templates;
+using GraphQL.Subscription;
+using GraphQL.Types;
+using System;
+using System.Threading.Tasks;
+
+namespace Tests.Adapters
+{
+    public class EventStreamResolverTest : TestBase
+    {
+        [Test]
+        public void Can_Resolve_Value()
+        {
+            var result = "testring";
+            var context = new ResolveFieldContext { Source = result };
+            var resolver = new EventStreamResolver(null);
+
+            Assert.AreEqual(result, resolver.Resolve(context));
+        }
+
+        [Test]
+        public async Task Can_Subscribe()
+        {
+            var engine = GraphQLEngine.New().WithSubscription<Subscription>();
+            var result = await engine
+                .NewExecutor()
+                .WithQueryString("subscription { test }")
+                .Execute();
+
+            Assert.AreEqual(1, ((SubscriptionExecutionResult)result).Streams.Count);
+        }
+    }
+
+    class Subscription
+    {
+        public IObservable<string> Test { get; }
+    }
+}
