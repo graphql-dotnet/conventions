23db689d55b618befea99dc50d6ff15c271355bc
Tommy Lillehagen <tommy@lillehagen.me>
2021-04-27 06:39:04 +0100

    Bump to 4.1.0 and attempt to fix #219

--- src/GraphQL.Conventions/Adapters/Types/IdGraphType.cs
+++ src/GraphQL.Conventions/Adapters/Types/IdGraphType.cs
@@ -6,8 +6,13 @@ namespace GraphQL.Conventions.Adapters.Types
     public class IdGraphType : GraphQL.Types.IdGraphType
     {
         /// <inheritdoc/>
-        public override object ParseValue(object value) => value != null 
-            ? new Id(value.ToString()) 
-            : null;
+        public override object ParseValue(object value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            return new Id(value.ToString());
+        }
     }
 }
