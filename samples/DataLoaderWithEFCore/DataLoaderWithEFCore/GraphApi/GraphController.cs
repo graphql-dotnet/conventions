c4b0555ed4f18cd39dc30211eeb5fcc74d080eb1
Shane Krueger <shane@acdmail.com>
2022-12-21 01:26:42 -0500

    Update samples/DataLoaderWithEFCore/DataLoaderWithEFCore/GraphApi/GraphController.cs
    Co-authored-by: Tommy Lillehagen <tommy@lillehagen.me>

--- samples/DataLoaderWithEFCore/DataLoaderWithEFCore/GraphApi/GraphController.cs
+++ samples/DataLoaderWithEFCore/DataLoaderWithEFCore/GraphApi/GraphController.cs
@@ -18,6 +18,7 @@ namespace DataLoaderWithEFCore.GraphApi
         public GraphController(GraphQLEngine engine, IServiceProvider serviceProvider)
         {
             _engine = engine;
+            _serviceProvider = serviceProvider;
         }
 
         [HttpPost]
