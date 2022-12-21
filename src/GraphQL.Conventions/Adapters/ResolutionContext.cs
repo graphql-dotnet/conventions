db5cc3ee1bfe62211570b34fa02749ab9416b53f
Shane32 <shane@acdmail.com>
2022-12-21 14:04:59 -0500

    Update formatting

--- src/GraphQL.Conventions/Adapters/ResolutionContext.cs
+++ src/GraphQL.Conventions/Adapters/ResolutionContext.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using GraphQL.Conventions.Execution;
 using GraphQL.Conventions.Types.Descriptors;
 using GraphQL.Execution;
 
